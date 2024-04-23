7a3f90efd03c9fbe68424ebf17df91891561cc55
FrancoBalduini <franco233433@gmail.com>
2024-04-23 15:14:46 -0300

    Subo practica obligatoria 3

---
+++ practicaobligatoria3/src/login/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+const Login = () => {
+  const [user, setUser] = useState("");
+
+  const InputChangeHandler = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue.includes("o")) {
+      alert("Por favor, ¡Nombres de usuario sin la letra o!");
+    }
+    setUser(inputValue);
+    console.log(inputValue);
+  };
+
+  const RegisterHandler = () => {
+    console.log(user);
+    if (user.trim() === "" || user.toLowerCase().includes("o")) {
+      alert("Usuario inválido para la registración");
+    } else {
+      alert("Usuario registrado correctamente");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <input
+        type="text"
+        placeholder="Nombre de Usuario"
+        value={user}
+        onChange={InputChangeHandler}
+      />
+      <button onClick={RegisterHandler}>Registrarse</button>
+      <p>{user}</p>
+    </div>
+  );
+};
+
+export default Login;
